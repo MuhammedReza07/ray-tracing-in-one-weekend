87180ed82a00a72e3f21fa898421b6fa623d7dcf
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-05-31 22:26:29 +0200

    feat/hardcoded spheres (chapter 5).

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 use ray_tracing_in_one_weekend::{
     color,
-    ray,
+    ray::Ray,
     vector3::Vector3
 };
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Sphere {
+    center: Vector3,
+    radius: f64
+}
+
+impl Sphere {
+    pub fn new(center: Vector3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+
+    // Intersection test using the discriminant of the quadratic equation `(C - P) * (C - P) = R^2` where
+    // C is the centre of the sphere, P = Q + dt is a point on the ray, and R is the radius of the sphere.
+    pub fn hit(&self, ray: &Ray) -> bool {
+        let oc = self.center - ray.origin();
+        let a = ray.direction().norm2();
+        let b = -2.0 * Vector3::dot(ray.direction(), oc);
+        let c = oc.norm2() - self.radius;
+        b * b - 4.0 * a * c >= 0.0
+    }
+}
 
 fn main() {
     // Image.
@@ -27,6 +49,10 @@ fn main() {
     let viewport_delta_v = viewport_v / (IMAGE_HEIGHT as f64);
     let first_pixel_center = viewport_top_left + (viewport_delta_u + viewport_delta_v) / 2.0;
 
+    // Scene.
+    let sphere_1 = Sphere::new(Vector3::new(0.0, 4.0, 0.0), 0.5);
+    let sphere_2 = Sphere::new(Vector3::new(-2.5, 8.0, 0.0), 0.5);
+
     // Render.
     color::write_p3_header(IMAGE_WIDTH, IMAGE_HEIGHT, COLOR_DEPTH);
 
@@ -34,9 +60,16 @@ fn main() {
         eprintln!("Scan lines remaining: {}", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
             let pixel_center = first_pixel_center + (i as f64) * viewport_delta_u + (j as f64) * viewport_delta_v;
-            let ray = ray::Ray::new(camera_center, pixel_center - camera_center);
+            let ray = Ray::new(camera_center, pixel_center - camera_center);
             let t = (ray.direction().normalize().z() + 1.0) / 2.0;
-            let color = color::lerp(&color::Color::new(1.0, 1.0, 1.0), &color::Color::new(0.5, 0.7, 1.0), t);
+            let color: color::Color;
+            if sphere_1.hit(&ray) {
+                color = color::Color::new(1.0, 0.0, 0.0);
+            } else if sphere_2.hit(&ray) {
+                color = color::Color::new(0.0, 1.0, 0.0);
+            } else {
+                color = color::lerp(&color::Color::new(1.0, 1.0, 1.0), &color::Color::new(0.5, 0.7, 1.0), t);
+            }
             color::write_p3_color(&color, COLOR_DEPTH);
         }
     }
