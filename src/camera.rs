8d80b2da335b1855fb9ed0dbf9375e7d7f23f7ed
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-06-07 19:22:02 +0200

    feat/added anti-aliasing (chapter 8).

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,15 @@
 use crate::{
     color::*,
+    random,
     ray::Ray,
     renderable_list::RenderableList,
     vector3::Vector3
 };
+use rand::Rng;
 use std::f64;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Camera {
+pub struct Camera<R: Rng> {
     aspect_ratio: f64,
     camera_center: Vector3,
     color_depth: u32,
@@ -15,10 +17,12 @@ pub struct Camera {
     image_width: u32,
     viewport_width: f64,
     t_min: f64,
-    t_max: f64
+    t_max: f64,
+    samples_per_pixel: u32,
+    rng: R
 }
 
-impl Camera {
+impl<R: Rng> Camera<R> {
     pub fn new(
         aspect_ratio: f64,
         camera_center: Vector3,
@@ -27,7 +31,9 @@ impl Camera {
         image_width: u32,
         viewport_width: f64,
         t_min: f64,
-        t_max: f64
+        t_max: f64,
+        samples_per_pixel: u32,
+        rng: R
     ) -> Self {
         Self {
             aspect_ratio,
@@ -37,11 +43,13 @@ impl Camera {
             image_width,
             viewport_width,
             t_min,
-            t_max
+            t_max,
+            samples_per_pixel,
+            rng
         }
     }
 
-    pub fn render(&self, scene: &RenderableList) {
+    pub fn render(&mut self, scene: &RenderableList) {
         // Set additional image and camera parameters.
         // Ensure that image_height is at least 1.
         let image_height = if self.aspect_ratio > self.image_width as f64 { 1 } else { (self.image_width as f64 / self.aspect_ratio) as u32 };
@@ -57,27 +65,36 @@ impl Camera {
         let viewport_delta_v = viewport_v / (image_height as f64);
         let first_pixel_center = viewport_top_left + (viewport_delta_u + viewport_delta_v) / 2.0;
 
+        // Miscellaneous parameters.
+        // Radius of the disk used for anti-aliasing.
+        let anti_aliasing_disk_r = f64::max(viewport_delta_u.norm(), viewport_delta_v.norm());
+
         // Render.
         write_p3_header(self.image_width, image_height, self.color_depth);
 
         for j in 0..image_height {
             eprintln!("Scan lines remaining: {}", image_height - j);
             for i in 0..self.image_width {
-                let pixel_center = first_pixel_center + (i as f64) * viewport_delta_u + (j as f64) * viewport_delta_v;
-                let ray = Ray::new(camera_center, pixel_center - camera_center);
-                let color: Color;
-                
-                if let Some(intersection) = scene.intersect(&ray, self.t_min, self.t_max) {
-                    let p = ray.at(intersection.t);
-                    let object = scene.get(intersection.index);
-                    let n = object.normal(p);
-                    color = (Vector3::from([n.x(), n.z(), n.y()]) + Vector3::from([1.0; 3])) / 2.0;
-                } else {
-                    let t = (ray.direction().normalize().z() + 1.0) / 2.0;
-                    color = lerp(&Color::new(1.0, 1.0, 1.0), &Color::new(0.5, 0.7, 1.0), t);
+                let mut color_acc = Vector3::from([0.0; 3]);
+
+                for _ in 0..self.samples_per_pixel {
+                    let pixel_center = first_pixel_center + (i as f64) * viewport_delta_u + (j as f64) * viewport_delta_v;
+                    let disk_sample = random::sample_unit_disk_uniform(&mut self.rng);
+                    let ray_offset = anti_aliasing_disk_r * Vector3::new(disk_sample.x(), 0.0, disk_sample.y());
+                    let ray = Ray::new(camera_center, pixel_center - camera_center + ray_offset);
+                    
+                    if let Some(intersection) = scene.intersect(&ray, self.t_min, self.t_max) {
+                        let p = ray.at(intersection.t);
+                        let object = scene.get(intersection.index);
+                        let n = object.normal(p);
+                        color_acc += (Vector3::from([n.x(), n.z(), n.y()]) + Vector3::from([1.0; 3])) / 2.0;
+                    } else {
+                        let t = (ray.direction().normalize().z() + 1.0) / 2.0;
+                        color_acc += lerp(&Color::from([1.0; 3]), &Color::new(0.5, 0.7, 1.0), t);
+                    }
                 }
-                
-                write_p3_color(&color, self.color_depth);
+
+                write_p3_color(&(color_acc / self.samples_per_pixel as f64), self.color_depth);
             }
         }
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use ray_tracing_in_one_weekend::{
     surfaces::sphere::Sphere, 
     vector3::Vector3
 };
+use rand_pcg::Pcg64Mcg;
+
+// Set RNG parameters.
+const RNG_SEED: u128 = 0x323030372d30382d33314d696b753339;
 
 // Set image and camera parameters.
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
@@ -13,10 +17,14 @@ const IMAGE_WIDTH: u32 = 400;
 const VIEWPORT_WIDTH: f64 = 1.0;
 const T_MIN: f64 = 0.0;
 const T_MAX: f64 = f64::INFINITY;
+const SAMPLES_PER_PIXEL: u32 = 32;
 
 fn main() {
+    // RNG.
+    let rng = Pcg64Mcg::new(RNG_SEED);
+
     // Camera.
-    let camera = Camera::new(
+    let mut camera = Camera::new(
         ASPECT_RATIO, 
         Vector3::new(0.0, 0.0, 0.0),
         COLOR_DEPTH,
@@ -24,7 +32,9 @@ fn main() {
         IMAGE_WIDTH,
         VIEWPORT_WIDTH,
         T_MIN,
-        T_MAX
+        T_MAX,
+        SAMPLES_PER_PIXEL,
+        rng
     );
 
     // Scene.
@@ -33,5 +43,5 @@ fn main() {
     scene.push(Box::new(Sphere::new(Vector3::new(0.0, -3.0, -100.5), 100.0)));
 
     // Render.
-    camera.render(&scene);
+    camera.render(&mut scene);
 }
