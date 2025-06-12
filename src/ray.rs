95a6781b33e03559c275aecb228e2d247637fa44
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-06-12 20:32:13 +0200

    feat/added functions for computing the length of a ray time t.

--- src/ray.rs
+++ src/ray.rs
@@ -14,4 +14,8 @@ impl Ray {
     pub fn at(&self, t: f64) -> Vector3 {
         self.origin + t * self.direction
     }
+
+    pub fn length(&self, t: f64) -> f64 {
+        (self.at(t) - self.origin).norm()
+    }
 }
